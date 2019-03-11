df3018ca4ece30ee0fc50f72d78dacad013fa928
manishsxtha <manish.xshrestha@gmail.com>
2019-03-11 15:54:06 +0545

    Linear layout - weight sum for responsive design - inserting images and drawables provided by android library

---
+++ app/src/main/java/com/manish/kathford/FacebookActivityv2.java
@@ -0,0 +1,14 @@
+package com.manish.kathford;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookActivityv2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_facebook_activityv2);
+    }
+}
